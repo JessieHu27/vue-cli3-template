5b54903b27c16cbd6095a46a569424371ee3cf15
fchu <fchu@easegroup.com>
2020-01-14 14:02:49 +0800

    修复dll路径

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
 /*dll 打包路径*/
-exports.DLL_DIR = path.resolve(__dirname, './.dll')
+exports.DLL_DIR = path.join(__dirname, './.dll')
 /*开启gzip 压缩*/
 exports.GZIP_ON = false
